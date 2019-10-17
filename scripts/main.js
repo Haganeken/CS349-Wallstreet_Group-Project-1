ee4fabef32282f3ddf598d14655deb23f2be9a8f
Jenny-Reinhard <55107673+Jenny-Reinhard@users.noreply.github.com>
2019-10-17 13:20:30 -0700

    stick navigation bar to top

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,22 @@ output.innerHTML = slider.value; // Display the default slider value
 slider.oninput = function () {
     output.innerHTML = this.value;
 };
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {toggleSticky()};
+
+// Get the navbar
+var navbar = document.getElementById("filterbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function toggleSticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
