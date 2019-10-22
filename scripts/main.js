388663786a4d8ae0be4b7b9af43a2e0f1d11fff7
simonmoesorensen <moe.simon@gmail.com>
2019-10-21 22:31:53 -0700

    Added preview functionality to image upload Moved filterbar styling to own file Retouched modal UI

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,20 @@ $(document).ready(function () {
     initFilterBar();
 });
 
+$('#image').change(function () {
+    readURL(this);
+});
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        let $uploadedImage = $('#uploadedImage');
+        reader.onload = function (e) {
+            $uploadedImage.attr('src', e.target.result);
+            $uploadedImage.removeClass("d-none");
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
